00f51c65ed5086ebe89ef0fbfe9062556294b60d
Junior Rosa <acsrosajunior@gmail.com>
2022-12-01 20:23:23 -0300

    Adicionando as funções para calcular o IMC, faixas de mensagens.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,61 @@
-const form = document.querySelector('#form');
+const form = document.querySelector('#formulario');
+
 
 form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const inputPeso = e.target.querySelector('#peso');
-    const inputAltura = e.target.querySelector('#altura');
-
-    const peso = Number(inputPeso.value);
-    const altura = Number(inputAltura.value);
+  e.preventDefault();
+  //console.log('Evento Previnido.');
+  //setResultado('Olá Mundo.')
+  let peso = document.getElementById("peso").value;
+  let altura = document.getElementById("altura").value;
+  calcularImc(peso, altura);
 });
 
-function criaP () {
-    const p = document.createElement('p');
-    return p;
+
+
+function calcularImc(peso, altura) {
+
+  let resultado = peso / (altura * altura);
+  let resultadoFormatado = resultado.toFixed(2);
+
+  setResultado(resultadoFormatado);
+ 
 }
 
-function setResultado (msg) {
-    const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = '';
-    
-   const p = criaP();
+function setResultado(msg) {
+  const resultado = document.querySelector('#resultado');
+  let infoAdicional = "Fora de Faixa";
+
+  if(msg < 18.5) {
+      infoAdicional ="Abaixo do Peso. ";
+  }
+
+  if(msg > 18.5 && msg < 24.9) {
+    infoAdicional ="Peso Normal. ";
+
+  }
+
+  if(msg > 25 && msg < 29.9) {
+    infoAdicional ="Sobrepeso. ";
+
+  }
+
+  if(msg > 30 && msg < 34.9) {
+    infoAdicional ="Obesidade Grau 1. ";
+
+  }
+
+  if(msg > 35 && msg < 39.9) {
+    infoAdicional ="Obesidade Grau 2 ";
+
+  }
+
+  if(msg > 40) {
+    infoAdicional ="Obesidade Grau 3 "; 
+
+  }  
+  resultado.innerHTML = '<b>Resultado: </b>' + msg + " --" + infoAdicional;
+}
+
+function limpar(){
+  alert('Cheguei');
 }
