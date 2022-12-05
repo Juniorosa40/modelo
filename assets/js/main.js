133139eb0bbfc5b9f37092d1653b49d865763a55
Junior Rosa <acsrosajunior@gmail.com>
2022-12-05 10:54:06 -0300

    Finalizamos nosso formulario adicionando as melhorias que faltavam e também o resultado quando for impresso será destacado dentro de uma caixa com a cor verde.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,66 @@
+// Capturar evento de submit do formulário
 const form = document.querySelector('#formulario');
-
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-  //console.log('Evento Previnido.');
-  //setResultado('Olá Mundo.')
-  let peso = document.getElementById("peso").value;
-  let altura = document.getElementById("altura").value;
-  calcularImc(peso, altura);
+  const inputPeso = e.target.querySelector('#peso');
+  const inputAltura = e.target.querySelector('#altura');
+
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
+
+  if (!peso) {
+    setResultado('Peso inválido', false);
+    return;
+  }
+
+  if (!altura) {
+    setResultado('Altura inválida', false);
+    return;
+  }
+
+  const imc = getImc(peso, altura);
+  const nivelImc = getNivelImc(imc);
+
+  const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+
+  setResultado(msg, true);
 });
 
+function getNivelImc (imc) {
+  const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
+    'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
 
-
-function calcularImc(peso, altura) {
-
-  let resultado = peso / (altura * altura);
-  let resultadoFormatado = resultado.toFixed(2);
-
-  setResultado(resultadoFormatado);
- 
+  if (imc >= 39.9) return nivel[5];
+  if (imc >= 34.9) return nivel[4];
+  if (imc >= 29.9) return nivel[3];
+  if (imc >= 24.9) return nivel[2];
+  if (imc >= 18.5) return nivel[1];
+  if (imc < 18.5) return nivel[0];
 }
 
-function setResultado(msg) {
+function getImc (peso, altura) {
+  const imc = peso / altura ** 2;
+  return imc.toFixed(2);
+}
+
+function criaP () {
+  const p = document.createElement('p');
+  return p;
+}
+
+function setResultado (msg, isValid) {
   const resultado = document.querySelector('#resultado');
-  let infoAdicional = "Fora de Faixa";
+  resultado.innerHTML = '';
 
-  if(msg < 18.5) {
-      infoAdicional ="Abaixo do Peso. ";
+  const p = criaP();
+
+  if (isValid) {
+    p.classList.add('paragrafo-resultado');
+  } else {
+    p.classList.add('bad');
   }
 
-  if(msg > 18.5 && msg < 24.9) {
-    infoAdicional ="Peso Normal. ";
-
-  }
-
-  if(msg > 25 && msg < 29.9) {
-    infoAdicional ="Sobrepeso. ";
-
-  }
-
-  if(msg > 30 && msg < 34.9) {
-    infoAdicional ="Obesidade Grau 1. ";
-
-  }
-
-  if(msg > 35 && msg < 39.9) {
-    infoAdicional ="Obesidade Grau 2 ";
-
-  }
-
-  if(msg > 40) {
-    infoAdicional ="Obesidade Grau 3 "; 
-
-  }  
-  resultado.innerHTML = '<b>Resultado: </b>' + msg + " --" + infoAdicional;
-}
-
-function limpar(){
-  alert('Cheguei');
+  p.innerHTML = msg;
+  resultado.appendChild(p);
 }
